884fd6dd03d4ca037098d3a77da192637075eee9
Andrei Iacob <andreiiacob@microsoft.com>
2021-11-12 13:54:09 -0800

    Add context

--- packages/utils/telemetry-utils/src/index.ts
+++ packages/utils/telemetry-utils/src/index.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+export * from "./context";
 export * from "./debugLogger";
 export * from "./errorLogging";
 export * from "./eventEmitterWithErrorHandling";
