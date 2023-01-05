8173b8ee8874f0238502ef05c07d8012809f208d
Andrei Iacob <andreiiacob@microsoft.com>
2023-01-05 14:32:54 -0800

    Use detached container

--- packages/test/test-end-to-end-tests/src/test/opOrdering.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/opOrdering.spec.ts
@@ -23,6 +23,7 @@ const versions = [
     "2.0.0-internal.2.2.0",
     "2.0.0-internal.2.1.0",
     "2.0.0-internal.2.0.0",
+    "2.0.0-internal.1.4.8",
 ];
 
 describeInstallVersions(
@@ -33,75 +34,72 @@ describeInstallVersions(
 )(
     "OP Ordering",
     (getTestObjectProvider) => {
-        let provider: ITestObjectProvider;
-        let oldMap: SharedMap;
-        let newMap: SharedMap;
-        let oldContainer: IContainer;
-        beforeEach(() => {
-            provider = getTestObjectProvider();
-        });
-        afterEach(async () => provider.reset());
-
-        const innerRequestHandler = async (request: IRequest, runtime: IContainerRuntimeBase) =>
-            runtime.IFluidHandleContext.resolveHandle(request);
-        const mapId = "map";
-        const registry: ChannelFactoryRegistry = [
-            [mapId, SharedMap.getFactory()],
-        ];
-        const factory: TestFluidObjectFactory = new TestFluidObjectFactory(
-            registry,
-            "default",
-        );
-        const testContainerConfig: ITestContainerConfig = {
-            fluidDataObjectType: DataObjectFactoryType.Test,
-            registry,
-        };
-
-        const createOldContainer = async (version): Promise<IContainer> => {
-            const oldContainerRuntimeFactoryWithDefaultDataStore =
-                getContainerRuntimeApi(version).ContainerRuntimeFactoryWithDefaultDataStore;
-            const oldRuntimeFactory =
-                new oldContainerRuntimeFactoryWithDefaultDataStore(
-                    factory,
-                    [
-                        [factory.type, Promise.resolve(factory)],
-                    ],
-                    undefined,
-                    [innerRequestHandler],
-                    {
-                        gcOptions: {
-                            gcAllowed: true,
-                        },
-                    },
-                );
-
-            return provider.createContainer(oldRuntimeFactory);
-        };
-
-        const setupContainers = async (version: string) => {
-            oldContainer = await createOldContainer(version);
-            const oldDataObject = await requestFluidObject<ITestFluidObject>(oldContainer, "default");
-            oldMap = await oldDataObject.getSharedObject<SharedMap>(mapId);
-
-            const containerOnLatest = await provider.loadTestContainer(testContainerConfig);
-            const newDataObject = await requestFluidObject<ITestFluidObject>(containerOnLatest, "default");
-            newMap = await newDataObject.getSharedObject<SharedMap>(mapId);
-
-            await provider.ensureSynchronized();
-        };
-
         describe("OP processing", () => versions.forEach((version) => {
+            let provider: ITestObjectProvider;
+            let map: SharedMap;
+            let detachedContainer: IContainer;
+            beforeEach(() => {
+                provider = getTestObjectProvider();
+            });
+            afterEach(async () => provider.reset());
+
+            const innerRequestHandler = async (request: IRequest, runtime: IContainerRuntimeBase) =>
+                runtime.IFluidHandleContext.resolveHandle(request);
+            const mapId = "map";
+            const registry: ChannelFactoryRegistry = [
+                [mapId, SharedMap.getFactory()],
+            ];
+            const factory: TestFluidObjectFactory = new TestFluidObjectFactory(
+                registry,
+                "default",
+            );
+            const testContainerConfig: ITestContainerConfig = {
+                fluidDataObjectType: DataObjectFactoryType.Test,
+                registry,
+            };
+
+            const createOldDetachedContainer = async (version): Promise<IContainer> => {
+                const oldContainerRuntimeFactoryWithDefaultDataStore =
+                    getContainerRuntimeApi(version).ContainerRuntimeFactoryWithDefaultDataStore;
+                const oldRuntimeFactory =
+                    new oldContainerRuntimeFactoryWithDefaultDataStore(
+                        factory,
+                        [
+                            [factory.type, Promise.resolve(factory)],
+                        ],
+                        undefined,
+                        [innerRequestHandler],
+                        {
+                            gcOptions: {
+                                gcAllowed: true,
+                            },
+                        },
+                    );
+
+                const loader = provider.createLoader([[provider.defaultCodeDetails, oldRuntimeFactory]]);
+                return loader.createDetachedContainer(provider.defaultCodeDetails);
+            };
+
+            const setupContainers = async (version: string) => {
+                const startup = await provider.makeTestContainer(testContainerConfig);
+                const startupDO = await requestFluidObject<ITestFluidObject>(startup, "default");
+                await startupDO.getSharedObject<SharedMap>(mapId);
+
+                detachedContainer = await createOldDetachedContainer(version);
+                const oldDataObject = await requestFluidObject<ITestFluidObject>(detachedContainer, "default");
+                map = await oldDataObject.getSharedObject<SharedMap>(mapId);
+            };
+
             it(`Ops are processed in order, version: ${version}`, async () => {
                 await setupContainers(version);
-                oldContainer.once("connected", () => {
-                    oldMap.set("key", "latest");
+                map.set("key", "oldest");
+                detachedContainer.once("connected", () => {
+                    map.set("key", "latest");
                 });
-                oldContainer.disconnect();
-                oldMap.set("key", "oldest");
-                oldContainer.connect();
-
+                await detachedContainer.attach(provider.driver.createCreateNewRequest(provider.documentId));
                 await provider.ensureSynchronized();
-                assert.equal(newMap.get("key"), "latest");
+
+                assert.equal(map.get("key"), "latest");
             });
         }));
     });
